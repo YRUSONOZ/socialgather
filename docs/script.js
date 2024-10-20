c841bc4e56654387b09d489c318db17d395f1d16
YRUSONOZ <74988686+YRUSONOZ@users.noreply.github.com>
2024-10-20 00:05:19 -0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,11 @@
 // Initialize Firebase
 const firebaseConfig = {
-  // Your Firebase configuration goes here
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
